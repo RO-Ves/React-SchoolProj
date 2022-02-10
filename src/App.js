459b91419506d8441485db0a767be4027e1363f6
RO-Ves <joel.eriksson@ubf.haninge.se>
2022-02-10 12:21:28 +0100

    1 file modified, 1 added, 3 deleted

--- src/App.js
+++ src/App.js
@@ -38,42 +38,14 @@ function Test() {
   );
 }
 
-function Intro() {
-  const styling = useSpring({
-    from: { y: 0, x: 0 },
-    to: { x: 550, y: 250 },
-  });
-
-  return (
+function Intro(){
+  return(
     <div>
-      <div className="row align-items-center move-me">
-        <animated.div
-          style={{ styling }}
-          className="col d-flex justify-content-center align-self-center"
-        >
-          <TypeWriterEffect
-            className="typeWriter"
-            textStyle={{
-              fontFamily: 'montserrat, sans-serif',
-              color: '#3F3D56',
-              fontWeight: 500,
-              fontSize: '4vw',
-              borderBottom: '5px solid',
-            }}
-            startDelay={2000}
-            cursorColor="#3F3D56"
-            multiText={[
-              'Hey there, this is the assignment you gave me',
-              'I didnt know how to make an intro so I did a',
-              'TypeWriterEffect animation thing yay!!',
-            ]}
-            multiTextDelay={1000}
-            typeSpeed={30}
-          />
-        </animated.div>
-      </div>
+      <video loop autoPlay muted id="background">
+        <source src={require('./assets/neon-background.mp4')}/>
+      </video>
     </div>
-  );
+  )
 }
 
 export default function App() {
